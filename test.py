9ecab9c8f748964fb6ddc5bd8cff58139164ef7f
saoski <saoski88@gmail.com>
2025-02-22 16:51:32 -0500

    Got response from chatbot

--- test.py
+++ test.py
@@ -3,28 +3,46 @@ import datetime
 import getpass
 import os
 import uuid
+from mimetypes import guess_type
 
 import pyscreenshot as ImageGrab
 from langchain.tools import tool
 from langchain_community.chat_models import ChatOpenAI
-from langchain_core.messages import HumanMessage
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.prompts import ChatPromptTemplate, \
+    HumanMessagePromptTemplate
 
 from dotenv import load_dotenv
 
 import requests
 import PIL
+from langchain_core.prompts.image import ImagePromptTemplate
 
 load_dotenv()
 key = os.getenv("OPENAI_KEY")
 
-# @tool
-hint_prompt_template = ChatPromptTemplate([
-    ("system", "You are a helpful math tutor that will get hints any explain concepts in any problems you are asked about"),
+hint_prompt_template = ChatPromptTemplate([("system",
+                                            "You are a helpful math tutor that will get hints any explain concepts in any problems you are asked about"),
     ("human", "I am struggling with understanding this problem:\n"
               "{problem_description}\n"
-              "Can you give me advice?")
-])
+              "Can you give me advice?")])
+
+
+# Function to encode a local image into data URL
+def local_image_to_data_url(image_path):
+    mime_type, _ = guess_type(image_path)
+    # Default to png
+    if mime_type is None:
+        mime_type = 'image/png'
+
+    # Read and encode the image file
+    with open(image_path, "rb") as image_file:
+        base64_encoded_data = base64.b64encode(image_file.read()).decode(
+            'utf-8')
+
+    # Construct the data URL
+    return f"data:{mime_type};base64,{base64_encoded_data}"
+
 
 # @tool
 def take_screenshot():
@@ -44,20 +62,12 @@ def generate_summary(image_url, prompt):
     model = ChatOpenAI(model="gpt-4o", max_tokens=1024)
 
     # Create a message with the image
-    message = model.invoke(
-        [
-        HumanMessage(
-            content=[
-                    {"type": "text", "text": prompt},
-                    {
-                        "type": "image_url",
-                        "image_url": {"url": image_url},
-                    },
-                ]
-            )
-        ]
-    )
+    message = model.invoke([HumanMessage(
+        content=[{"type": "text", "text": prompt},
+            {"type": "image_url", "image_url": {"url": image_url}, }])])
+
     return message.content
+
 
 def main():
     # Setup the API Key for OpenAI
@@ -69,18 +79,18 @@ def main():
 
     project_name = f"openai_image_summarization_{current_time}"
     os.environ["LANGCHAIN_TRACING_V2"] = "true"
-    os.environ["LANGCHAIN_PROJECT"]=project_name
+    os.environ["LANGCHAIN_PROJECT"] = project_name
 
     # Take a Screenshot
     screenshot = take_screenshot()
-    screenshot.save("screenshots/screenshot.png")
-    url = r"screenshot.png"
+    screenshot_file_path = r"screenshots/screenshot.png"
+    screenshot.save(screenshot_file_path)
+    image_data_url = local_image_to_data_url(screenshot_file_path)
 
     # # # Get the image summary
     prompt = """What's in this image?"""
-    res = generate_summary(url, prompt)
+    res = generate_summary(image_data_url, prompt)
     print(res)
-
 
 
 if __name__ == '__main__':
